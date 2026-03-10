53a6cb2c239536169b40fb255777841b44766821
mustafo <mustafopolatov9@gmail.com>
2026-03-10 15:39:23 +0500

    added

---
+++ 2-dars.js
@@ -0,0 +1,29 @@
+// Node.js -js kodlani terminalda gorish uchun ishlatiladi
+//node -v - versiyasini korsatadi
+//node 2-dars.js - faylni ishga tushiradi  
+//node --watch  js - faylni avtomatik ozgartirishcd uchun ishlatiladi 
+
+
+console.log('test');
+
+console.log('hello world');
+
+console.log('live');
+
+console.log(12345);
+console.log(12345);
+console.log(12345);
+
+//Browserda javascript funksiyalari -BOM - Broweser Object Model
+
+// prompt('Ismingizni kiriting'); 
+
+// alert('Xush kelibsiz');
+
+// confirm('Rostdana?');
+
+
+
+
+//DOM - Document Object Model
+
