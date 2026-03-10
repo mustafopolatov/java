7276a571abd92690ebc7cfd95da6a49ca9b92c15
mustafo <mustafopolatov9@gmail.com>
2026-03-10 15:37:05 +0500

    added

---
+++ 1-dars.js
@@ -0,0 +1,32 @@
+// Foydalanuvchidan ma'lumot olish
+
+// prompt('Ismingizni yozing') // string
+// confirm('15 yoshdan kattamisiz?')// boolean
+
+// Foydalanuvchiga ma'lumot berish
+
+// alert('well good')
+// console.log('privet');
+
+
+// let userName = 'alisher' 
+// console.log(userName);
+// userName = 'Rozmat'
+
+
+
+// console.log(userName); //ozgaruvchi
+
+
+
+// const userBirthday = 2011 //ozgarmas
+
+// console.log(userBirthday); 
+
+
+const userName = prompt('Ismingiz kim?')
+
+const userAge = prompt('Yoshingizni kiriting')
+
+
+alert(userName + ' siz '  +  (2026 - userAge)  + "-yilda tugilgansiz");
